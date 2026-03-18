bb9b80a1c4b1939d992a2bc992cd901f808a3720
kayz-web <kausalyas528@gmail.com>
2026-03-18 23:19:32 +0530

    Update example.py

--- Kausalya/example.py
+++ Kausalya/example.py
@@ -1,1 +1,16 @@
-print("Hello")
+def calculator_operations():
+    num1=float(input("Enter the first number:"))
+    num2=float(input("Enter the second number:"))
+    addition=num1+num2
+    subtraction=num1-num2
+    multiplication=num1*num2
+    if num2!=0:
+        division=num1/num2
+    else:
+        division="Error:Division by zero is not allowed"
+    print("\n---Calculator Results---")
+    print("Addition: ",addition)
+    print("Subtraction: ",subtraction)
+    print("Multiplication: ",multiplication)
+    print("Division: ",division)
+calculator_operations()
